b3f206345dd8ea70faaaa66c0ef6e42d8236876c
Miguel Maza <maza68206@gmail.com>
2026-06-15 07:49:44 -0500

    Ejercicios

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        runEjercicio1();
+
+    }
+    // Escribe una funcion recursiva que calcule la suma de 
+    // todos los numeros enteros consecutivos desde 1 hasta 
+    // a un numero dado n . Por ejemplo , si n es igual a 5 , 
+    // la funcion debe devolver 1+2+3+4+5 = 15 .
+    public static void runEjercicio1() {
+    
+    }
+}
